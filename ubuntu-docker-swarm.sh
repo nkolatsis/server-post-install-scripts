2a0f747981d75974f389b0b82fa3bc714ea45c55
Nicholas Kolatsis <13288906+nkolatsis@users.noreply.github.com>
2018-06-27 08:26:02 +0200

    Initial commit. Set up the idea.

---
+++ ubuntu-docker-swarm.sh
@@ -0,0 +1,18 @@
+# Author: Nicholas Kolatsis
+# URL: https://github.com/nkolatsis/postinstall-scripts/ubuntu-docker-swarm.sh
+# 
+# Description:
+# A post-install script for Ubuntu 18.04 (LTS) install that is hosted on
+# linode.com. The script will:
+#   - add a user with sudo access
+#   - change the hostname and hosts file
+#   - install docker
+#   - configure the server as a docker swarm manager or node as required
+
+
+
+user="nkolatsis"
+swarm="" # options: ["manager", "node"]
+auth_type="ssh-keys" # options: {"ssh-keys", "password"}
+hostname=""
+ssh_ip=""
